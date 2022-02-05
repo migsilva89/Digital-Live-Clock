4878d3362eef7cfeef102ac28b9cee39d8b47c03
migsilva89 <migdrum@gmail.com>
2022-02-05 18:28:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Digital Live Clock
+<br>
+
+![alt text](./screen.png)
+
+<br>
+
+## I made this project to practice:
+
+<br>
+
+- newDate()
+- setInterval()
+- toDateString()
+- toLocaleTimeString()
+- Functions
+- Dom Manipulation 
+- @import fonts to CSS
+
+<br>
+
+### More Projects:
+https://code-camp-responsive-wd-ld9d03x7e-migsilva89.vercel.app/
---
+++ index.js
@@ -0,0 +1,13 @@
+
+function updateTime() {
+
+    var timeNow = new Date().toLocaleTimeString()//Collect current hour
+    var time = document.getElementById('time')
+    time.innerHTML = timeNow
+
+    var dateNow = new Date()//Collect current day
+    var date = document.getElementById('date')
+    date.innerHTML = dateNow.toDateString() //Covert DATE FORMAT TO STRING ex: Sunday 15 Feb 2022
+}
+setInterval(updateTime,500)
+
